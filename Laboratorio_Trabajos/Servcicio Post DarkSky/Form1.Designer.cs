ecf3636b1c8af9d1700dea36a6d89cac58a34b22
Lara Nosti <laraqqqq53@gmail.com>
2021-10-02 07:02:29 +0900

    Trabajos anteriores

--- Laboratorio_Trabajos/Servcicio Post DarkSky/Form1.Designer.cs
+++ Laboratorio_Trabajos/Servcicio Post DarkSky/Form1.Designer.cs
@@ -430,7 +430,7 @@ namespace Servcicio_Post_DarkSky
             // 
             this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
             this.textBox1.ForeColor = System.Drawing.Color.Maroon;
-            this.textBox1.Location = new System.Drawing.Point(492, 46);
+            this.textBox1.Location = new System.Drawing.Point(492, 138);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 40;
@@ -439,7 +439,7 @@ namespace Servcicio_Post_DarkSky
             // 
             this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
             this.textBox2.ForeColor = System.Drawing.Color.Maroon;
-            this.textBox2.Location = new System.Drawing.Point(492, 72);
+            this.textBox2.Location = new System.Drawing.Point(492, 164);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 41;
@@ -449,7 +449,7 @@ namespace Servcicio_Post_DarkSky
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Lucida Sans Unicode", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
-            this.label1.Location = new System.Drawing.Point(422, 46);
+            this.label1.Location = new System.Drawing.Point(422, 138);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(64, 18);
             this.label1.TabIndex = 42;
@@ -460,7 +460,7 @@ namespace Servcicio_Post_DarkSky
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Lucida Sans Unicode", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
-            this.label2.Location = new System.Drawing.Point(407, 72);
+            this.label2.Location = new System.Drawing.Point(407, 164);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(79, 18);
             this.label2.TabIndex = 43;
@@ -470,7 +470,7 @@ namespace Servcicio_Post_DarkSky
             // 
             this.Coordenadas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.Coordenadas.ForeColor = System.Drawing.Color.Maroon;
-            this.Coordenadas.Location = new System.Drawing.Point(492, 94);
+            this.Coordenadas.Location = new System.Drawing.Point(492, 186);
             this.Coordenadas.Name = "Coordenadas";
             this.Coordenadas.Size = new System.Drawing.Size(100, 23);
             this.Coordenadas.TabIndex = 44;
